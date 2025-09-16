b0892a7b7083180ca484d3d3eb22a2afb7f4065c
VAISHNAVI SUNIL PURIGOSAVI <gosavivaishnavi9370@gmail.com>
2025-09-16 17:52:53 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,13 @@
+ # task
+num = int(input("Enter a number:"))
+if num %2 == 0:
+    print(num, "is Even")
+else:
+        print(num, " is odd")
+# task 2
+
+total = 0
+for i in range( 1, 51):
+    total += i
+
+print(" sum of integrs from 1 to 50 is:",total)
